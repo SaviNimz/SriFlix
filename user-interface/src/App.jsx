8fab2cda4755e061f2cdf45c90254520d68eb667
Savinu Aththanayake <107975408+SaviNimz@users.noreply.github.com>
2024-02-05 19:12:39 +0530

    first commit

---
+++ user-interface/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Sriflix from './pages/Sriflix';
+
+const App = () => {
+  return (
+    
+    <Router>
+
+      <Routes>
+
+        <Route path="/" element={<Sriflix/>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup/>} />
+        
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
---
+++ user-interface/src/pages/Sriflix.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Sriflix = () => {
+  return (
+    <div>Sflix</div>
+  )
+}
+
+export default Sriflix;
