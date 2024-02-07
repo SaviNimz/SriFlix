eca065e0a402cec48897e03d22a3744704e1a24f
Chemini <cheminimallikarachchi@gmail.com>
2024-02-07 09:25:07 +0530

    Navbar

--- user-interface/src/App.jsx
+++ user-interface/src/App.jsx
@@ -13,7 +13,7 @@ const App = () => {
 
         <Route path="/" element={<Sriflix/>} />
         <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup/>} />
+        <Route path="/signup" element={<Signup />} />
         
       </Routes>
     </Router>
