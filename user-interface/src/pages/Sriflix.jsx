e4bbedf3310a3aa38a8c4add0af5e6bb0fe1396f
Savinu Aththanayake <107975408+SaviNimz@users.noreply.github.com>
2024-02-16 13:35:29 +0530

    resolving conflicts #2

--- user-interface/src/pages/Sriflix.jsx
+++ user-interface/src/pages/Sriflix.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
-import backgroundImage from "../assets/home1.jpg";
-import MovieLogo from "../assets/homeTitle.png";
+import backgroundImage from "../assets/home.jpg";
+import MovieLogo from "../assets/homeTitle.webp";
 import styled from "styled-components";
 
 import { FaPlay } from "react-icons/fa";
@@ -36,11 +36,86 @@ export const Sriflix = () => {
 
 
   return(
-    <div>
+    <Container>
     <Navbar isScrolled={isScrolled} />
-    <Slider movies={movies} />
+    <div className="hero">
+      <img
+        src={backgroundImage}
+        alt="background"
+        className="background-image"
+      />
+      <div className="container">
+        <div className="logo">
+          <img src={MovieLogo} alt="Movie Logo" />
+        </div>
+        <div className="buttons flex">
+        <button
+            onClick={() => navigate("/player")}
+            className="flex j-center a-center"
+          >
+            <FaPlay />
+            Play
+          </button>
+          <button className="flex j-center a-center">
+            <AiOutlineInfoCircle />
+            More Info
+          </button>
+        </div>
+      </div>
+     
     </div>
+    <Slider movies={movies} />
+  </Container>
   );
 }
 
+const Container = styled.div`
+  background-color: black;
+  .hero {
+    position: relative;
+    .background-image {
+      filter: brightness(60%);
+    }
+    img {
+      height: 100vh;
+      width: 100vw;
+    }
+    .container {
+      position: absolute;
+      bottom: 5rem;
+      .logo {
+        img {
+          width: 100%;
+          height: 100%;
+          margin-left: 5rem;
+        }
+      }
+      .buttons {
+        margin: 5rem;
+        gap: 2rem;
+        button {
+          font-size: 1.4rem;
+          gap: 1rem;
+          border-radius: 0.2rem;
+          padding: 0.5rem;
+          padding-left: 2rem;
+          padding-right: 2.4rem;
+          border: none;
+          cursor: pointer;
+          transition: 0.2s ease-in-out;
+          &:hover {
+            opacity: 0.8;npm run dev
+          }
+          &:nth-of-type(2) {
+            background-color: rgba(109, 109, 110, 0.7);
+            color: white;
+            svg {
+              font-size: 1.8rem;
+            }
+          }
+        }
+      }
+    }
+  }
+`;
 export default Sriflix;
