16b6a4c82cdad6e6211a0845c4861e7b1b9817d9
Savinu Aththanayake <107975408+SaviNimz@users.noreply.github.com>
2024-02-29 11:44:31 +0530

    added route to get liked movies

--- api/routes/UserRoutes.js
+++ api/routes/UserRoutes.js
@@ -3,5 +3,6 @@ const { addToLikedMovies } = require("../controllers/UserController");
   const router = require("express").Router();
   
   router.post("/add", addToLikedMovies);
+  router.get("/liked/:email", getLikedMovies);
   
   module.exports = router;
