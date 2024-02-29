bf3c9d5c461b956ef2887a34eaa2df84d2736fe9
Savinu Aththanayake <107975408+SaviNimz@users.noreply.github.com>
2024-02-29 15:46:58 +0530

    fixed bugs on liked movies

--- api/routes/UserRoutes.js
+++ api/routes/UserRoutes.js
@@ -1,8 +1,12 @@
-const { addToLikedMovies } = require("../controllers/UserController");
+const {
+  addToLikedMovies,
+  getLikedMovies,
+
+} = require("../controllers/UserController");
   
   const router = require("express").Router();
   
   router.post("/add", addToLikedMovies);
   router.get("/liked/:email", getLikedMovies);
-  
+
   module.exports = router;
