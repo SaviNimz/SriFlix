0d982178b8618740509d60f0b787f798a1afceab
Savinu Aththanayake <107975408+SaviNimz@users.noreply.github.com>
2024-06-02 11:36:38 +0530

    added environment variables for DB credentials

--- api/server.js
+++ api/server.js
@@ -1,23 +1,28 @@
 const express = require("express");
 const cors = require("cors");
-const mongoose = require("mongoose");
 const userRoutes = require("./routes/UserRoutes");
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb+srv://SaviYa:SaviYa1000!!@cluster0.rdqilok.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-{
+// acquiring the environment variable associated with the mongoDB credentials 
+
+require('dotenv').config();
+const mongoose = require("mongoose");
+
+// connecting to the database
+
+mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-
-}).then(()=>{
+}).then(() => {
     console.log("DB Connected");
+}).catch((err) => {
+    console.error("DB Connection Error: ", err);
 });
-
 
 
 app.use("/api/user", userRoutes);
 
 app.listen(5000, console.log("server started on port 5000"));
+
