5ba1463a0b065877ed07663fc5719171d115e68d
Savinu Aththanayake <107975408+SaviNimz@users.noreply.github.com>
2024-06-03 17:18:55 +0530

    refactored Movies.jsx

--- user-interface/src/pages/Movies.jsx
+++ user-interface/src/pages/Movies.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import Navbar from "../components/Navbar";
-import CardSlider from "../components/CardSlider";
 import { onAuthStateChanged } from "firebase/auth";
 import { firebaseAuth } from "../utils/firebase-config";
 import { useNavigate } from "react-router-dom";
@@ -12,35 +11,59 @@ import Slider from "../components/Slider";
 import NotAvailable from "../components/NotAvailable";
 
 function MoviePage() {
+  // State to track if the page is scrolled
   const [isScrolled, setIsScrolled] = useState(false);
+
+  // Redux selectors to get movies, genres, and genresLoaded status from the store
   const movies = useSelector((state) => state.netflix.movies);
   const genres = useSelector((state) => state.netflix.genres);
   const genresLoaded = useSelector((state) => state.netflix.genresLoaded);
 
+  // React Router hooks for navigation
   const navigate = useNavigate();
+
+  // Redux dispatch to send actions
   const dispatch = useDispatch();
 
+  // Fetch genres on component mount
   useEffect(() => {
     dispatch(getGenres());
-  }, []);
+  }, [dispatch]);
 
+  // Fetch movies when genres are loaded
   useEffect(() => {
     if (genresLoaded) {
       dispatch(fetchMovies({ genres, type: "movie" }));
     }
-  }, [genresLoaded]);
+  }, [genresLoaded, dispatch, genres]);
 
+  // State to track the authenticated user
   const [user, setUser] = useState(undefined);
 
-  onAuthStateChanged(firebaseAuth, (currentUser) => {
-    if (currentUser) setUser(currentUser.uid);
-    else navigate("/login");
-  });
+  // Listen to Firebase authentication state changes
+  useEffect(() => {
+    onAuthStateChanged(firebaseAuth, (currentUser) => {
+      if (currentUser) {
+        setUser(currentUser.uid);
+      } else {
+        navigate("/login");
+      }
+    });
+  }, [navigate]);
 
-  window.onscroll = () => {
-    setIsScrolled(window.pageYOffset === 0 ? false : true);
-    return () => (window.onscroll = null);
-  };
+  // Handle window scroll event to set the isScrolled state
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.pageYOffset !== 0);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    // Clean up the event listener on component unmount
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
     <Container>
@@ -55,6 +78,7 @@ function MoviePage() {
   );
 }
 
+// Styled-components for the container
 const Container = styled.div`
   .data {
     margin-top: 8rem;
@@ -65,4 +89,5 @@ const Container = styled.div`
     }
   }
 `;
+
 export default MoviePage;
